a263358776ea23cf7b523bbe981da2cb8132e406
martinryder30 <martinryder30@gmail.com>
2026-06-13 11:34:25 +0530

    Update schemas.py

--- backemed/schemas.py
+++ backemed/schemas.py
@@ -1,1 +1,18 @@
 
+from pydantic import BaseModel
+
+
+class RequestCreate(BaseModel):
+    name: str
+    phone: str
+    request_type: str
+    location: str
+
+
+class RequestResponse(RequestCreate):
+    id: int
+    priority: str
+    status: str
+
+    class Config:
+        from_attributes = True
